7cdc28f9ca97cfb76da5895e10614d987397dcda
Jesse Farmer <jesse@codeunion.io>
2014-10-03 16:50:13 -0700

    Initial commit

---
+++ lib/rpn_expression.rb
@@ -0,0 +1,16 @@
+class RPNExpression
+  # Returns an object representing the supplied RPN expression
+  #
+  # @param expr [String] an RPN expression, e.g., "5 4 +"
+  def initialize(expr)
+    @expr = expr
+  end
+
+  # Evaluates the underlying RPN expression and returns the final result as a
+  # number.
+  #
+  # @return [Numeric] the evaluated RPN expression
+  def evaluate
+    "Implement RPNExpression#evaluate in #{__FILE__}"
+  end
+end
